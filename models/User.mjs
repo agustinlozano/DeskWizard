1276a5a73d2463b197c14d5b127ef78dd7e67bfe
Agustin Lozano Blua <agustinlozanoblua@gmail.com>
2022-06-12 11:11:50 -0300

    Add the first model: User

---
+++ models/User.mjs
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose'
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Please add a name']
+    },
+    username: {
+      type: String,
+      required: [true, 'Please add a username']
+    },
+    email: {
+      type: String,
+      required: [true, 'Please add an email'],
+      unique: true
+    },
+    password: {
+      type: String,
+      required: [true, 'Please add a password']
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false
+    }
+  },
+  {
+    timestamps: true
+  }
+)
+
+export default mongoose.model('User', userSchema)
