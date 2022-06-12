ada76d68b95d50bb996ae13527ded10450e203c2
Agustin Lozano Blua <agustinlozanoblua@gmail.com>
2022-06-12 11:19:59 -0300

    Finally, update the server and deploy users to the app

--- server.mjs
+++ server.mjs
@@ -1,7 +1,9 @@
+import dotenv from 'dotenv'
 import express from 'express'
 import connectDB from './config/db.mjs'
 import logger from './utils/logger.js'
-import dotenv from 'dotenv'
+import userRoutes from './routes/userRouter.mjs'
+import requestLogger from './middleware/requestLogger.mjs'
 dotenv.config()
 
 // Connect to database
@@ -9,10 +11,16 @@ connectDB()
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(requestLogger)
+
 // Routes
 app.get('/', (req, res) => {
   res.status(200).send('<h1>Hello, World!</h1>')
 })
+
+app.use('/api/users', userRoutes)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => logger.info(`Server started on port ${PORT}`))
