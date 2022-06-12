bee5a2faa7fcffb77d043e919f76a3e932228771
Agustin Lozano Blua <agustinlozanoblua@gmail.com>
2022-06-12 11:15:44 -0300

    Add authorization middleware for private routes

---
+++ middleware/authorization.mjs
@@ -0,0 +1,34 @@
+import User from '../models/User.mjs'
+import jwt from 'jsonwebtoken'
+import asyncHandler from 'express-async-handler'
+
+const protect = asyncHandler(async (req, res, next) => {
+  let token
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    try {
+      // Get token from header
+      token = req.headers.authorization.split(' ')[1]
+      // Verify token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET)
+      // Get user from token without password
+      req.user = await User.findById(decoded.id).select('-password')
+
+      next()
+    } catch (error) {
+      console.log(error)
+      res.status(401)
+      throw new Error('Not authorized')
+    }
+  }
+
+  if (!token) {
+    res.status(401)
+    throw new Error('Not authorized')
+  }
+})
+
+export { protect }
