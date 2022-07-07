2867becb46cd1d443b1a159a399fcf4871ec1424
Agustin Lozano Blua <agustinlozanoblua@gmail.com>
2022-07-07 09:40:08 -0300

    Add a react app using vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App () {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <p>Hello Vite + React!</p>
+        <p>
+          Edit <code>App.jsx</code> and save to test HMR updates.
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App
