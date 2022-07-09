907e6bc6154ecdfdfcc6e5d8abfb8a3ed69afe4e
Agustin Lozano Blua <agustinlozanoblua@gmail.com>
2022-07-09 11:13:41 -0300

    Add a new page for register

---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,80 @@
+import { FaUser } from 'react-icons/fa'
+
+const Register = () => {
+  const onSubmit = event => {
+
+  }
+
+  const onChange = event => {
+
+  }
+
+  return (
+    <>
+      <section className='heading'>
+        <h1>
+          <FaUser /> Register
+        </h1>
+        <p>Please create an account</p>
+      </section>
+
+      <section className='form'>
+        <form onSubmit={onSubmit}>
+          <div className='form-group'>
+            <input
+              type='text'
+              className='form-control'
+              id='name'
+              name='name'
+              value='aName'
+              onChange={onChange}
+              placeholder='Enter your name'
+              required
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='email'
+              className='form-control'
+              id='email'
+              name='email'
+              value='anEmail'
+              onChange={onChange}
+              placeholder='Enter your email'
+              required
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              className='form-control'
+              id='password'
+              name='password'
+              value='aPassword'
+              onChange={onChange}
+              placeholder='Enter password'
+              required
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              className='form-control'
+              id='password2'
+              name='password2'
+              value='aPassword2'
+              onChange={onChange}
+              placeholder='Confirm password'
+              required
+            />
+          </div>
+          <div className='form-group'>
+            <button className='btn btn-block'>Submit</button>
+          </div>
+        </form>
+      </section>
+    </>
+  )
+}
+
+export default Register
