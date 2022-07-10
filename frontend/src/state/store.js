3c03cea31ef5cb299a57787005b499ae8ca75c3d
Agustin Lozano Blua <agustinlozanoblua@gmail.com>
2022-07-10 12:02:00 -0300

    Update the store

--- frontend/src/state/store.js
+++ frontend/src/state/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
+import authReducer from '../features/auth/authSlice'
 
 export const store = configureStore({
   reducer: {
-    name: () => { return true }
+    auth: authReducer
   }
 })
