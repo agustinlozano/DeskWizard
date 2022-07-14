cca93905b59e196ce54376cfba608e97e40b9985
Agustin Lozano Blua <agustinlozanoblua@gmail.com>
2022-07-14 09:51:28 -0300

    Add a new page to catch unknown endpoints

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import Header from './components/Header'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import PageNotFound from './pages/PageNotFound'
 import Footer from './components/Footer'
 import 'react-toastify/dist/ReactToastify.css'
 
@@ -21,6 +22,7 @@ function App () {
             <Route path='/' element={<Home />} />
             <Route path='/login' element={<Login />} />
             <Route path='/register' element={<Register />} />
+            <Route path='*' element={<PageNotFound />} />
           </Routes>
         </div>
       </Router>
