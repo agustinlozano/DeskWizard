5bb314462b492cf2623d037d2cb51776a696e71a
Agustin Lozano Blua <agustinlozanoblua@gmail.com>
2022-07-14 19:27:33 -0300

    Edit component: now the images are getting from Imgur

--- frontend/src/components/Products/index.jsx
+++ frontend/src/components/Products/index.jsx
@@ -5,19 +5,19 @@ const Products = () => {
   const productData = [
     {
       name: 'Free',
-      path: 'src\\images\\free.png',
+      path: 'https://i.imgur.com/OFYo0fj.png',
       value: 0,
       description: 'Yes! You can provide tickets for your clients faster and free right here.'
     },
     {
       name: 'Pro',
-      path: 'src\\images\\pro.png',
+      path: 'https://i.imgur.com/lqTXfQc.png',
       value: 1,
       description: 'Take the power of a PRO and sell your products like a Jedi for just one dollar.'
     },
     {
       name: 'Business',
-      path: 'src\\images\\business.png',
+      path: 'https://i.imgur.com/LHo72G3.png',
       value: 3,
       description: 'You have many sales and clients, get this plan and build an empire!'
     }
