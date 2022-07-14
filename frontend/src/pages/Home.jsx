8d11c445a083607c8147f4c0a741cd8c632ebfde
Agustin Lozano Blua <agustinlozanoblua@gmail.com>
2022-07-13 22:21:17 -0300

    Add main html elment and the Product component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,21 +1,25 @@
 import { Link } from 'react-router-dom'
 import { FaQuestionCircle, FaTicketAlt } from 'react-icons/fa'
+import Products from '../component/Products'
 
 const Home = () => {
   return (
     <>
-      <section className='heading'>
-        <h1>What do you need help with?</h1>
-        <p>Please choose from an option below</p>
-      </section>
+      <main>
+        <section className='heading'>
+          <h1>What do you need help with?</h1>
+          <p>Please choose from an option below</p>
+        </section>
 
-      <Link to='/' className='btn btn-reverse btn-block'>
-        <FaQuestionCircle /> Create New Ticket
-      </Link>
+        <Link to='/' className='btn btn-reverse btn-block'>
+          <FaQuestionCircle /> Create New Ticket
+        </Link>
 
-      <Link to='/' className='btn btn-block'>
-        <FaTicketAlt /> View My Tickets
-      </Link>
+        <Link to='/' className='btn btn-block'>
+          <FaTicketAlt /> View My Tickets
+        </Link>
+      </main>
+      <Products />
     </>
   )
 }
