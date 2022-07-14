7e7c24f3b1e6e82d2e8a78bc4afc90ff320108e5
Agustin Lozano Blua <agustinlozanoblua@gmail.com>
2022-07-13 22:07:58 -0300

    Add a new component for product cards

---
+++ frontend/src/component/ProductCard/index.jsx
@@ -0,0 +1,28 @@
+import { Link } from 'react-router-dom'
+import { BiPurchaseTag } from 'react-icons/Bi'
+import './productCard.css'
+
+const ProductCart = ({ name, path, value, description }) => {
+  return (
+    <div className='product-card'>
+      <Link to='/'>
+        <img
+          src={path}
+          width='250'
+          height='210'
+        />
+      </Link>
+      <div className='product-info'>
+        <h3>{name}</h3>
+        <p>{description}</p>
+        <Link to='/'>
+          <div className='price'>
+            <span>$ {value} <BiPurchaseTag /></span>
+          </div>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default ProductCart
