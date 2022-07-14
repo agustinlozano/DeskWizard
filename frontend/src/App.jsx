e7e7552c6f393eae810dbfba2ddda8536c515dfb
Agustin Lozano Blua <agustinlozanoblua@gmail.com>
2022-07-14 19:16:49 -0300

    Update App: Add the new routes - NewTicket page - Tickets

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,12 +4,15 @@ import {
   Route
 } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
-import Header from './components/Header'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import PageNotFound from './pages/PageNotFound'
+import NewTicket from './pages/NewTicket'
+import Tickets from './pages/Tickets'
+import Header from './components/Header'
 import Footer from './components/Footer'
+import PrivateRoute from './components/PrivateRoute'
 import 'react-toastify/dist/ReactToastify.css'
 
 function App () {
@@ -22,6 +25,15 @@ function App () {
             <Route path='/' element={<Home />} />
             <Route path='/login' element={<Login />} />
             <Route path='/register' element={<Register />} />
+
+            <Route path='/new-ticket' element={<PrivateRoute />}>
+              <Route path='/new-ticket' element={<NewTicket />} />
+            </Route>
+
+            <Route path='/tickets' element={<PrivateRoute />}>
+              <Route path='/tickets' element={<Tickets />} />
+            </Route>
+
             <Route path='*' element={<PageNotFound />} />
           </Routes>
         </div>
