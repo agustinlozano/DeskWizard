8a9ce01ca55a11939847363b8299053e4409c505
Agustin Lozano Blua <agustinlozanoblua@gmail.com>
2022-07-14 09:51:28 -0300

    Add a new page to catch unknown endpoints

---
+++ frontend/src/pages/PageNotFound.jsx
@@ -0,0 +1,16 @@
+import { AiOutlineInfoCircle } from 'react-icons/ai'
+
+const PageNotFound = () => {
+  return (
+    <>
+      <section className='heading'>
+        <h2>
+          <AiOutlineInfoCircle /> Not found
+        </h2>
+        <p>Ups! It Seems there was an error.</p>
+      </section>
+    </>
+  )
+}
+
+export default PageNotFound
