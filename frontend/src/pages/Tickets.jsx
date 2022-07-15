650aa8e68dbe6b5df8b543e146cee31bd26e95b8
Agustin Lozano Blua <agustinlozanoblua@gmail.com>
2022-07-15 09:27:27 -0300

    Add the logic of Tickets component

--- frontend/src/pages/Tickets.jsx
+++ frontend/src/pages/Tickets.jsx
@@ -1,14 +1,38 @@
+import { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { reset, getTickets } from '../features/tickets/ticketSlice'
 import BackButton from '../components/BackButton'
 import TicketItem from '../components/TicketItem'
+import Spinner from '../components/Spinner'
 
 const Tickets = () => {
-  const tickets = []
+  const { tickets, isLoading, isSuccess } = useSelector(
+    (state) => state.tickets
+  )
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    return () => {
+      if (isSuccess) {
+        dispatch(reset())
+      }
+    }
+  }, [dispatch, isSuccess])
+
+  useEffect(() => {
+    dispatch(getTickets())
+  }, [dispatch])
+
+  if (isLoading) {
+    return <Spinner />
+  }
 
   return (
     <>
       <BackButton url='/' />
       <h1>Tickets</h1>
-      <div className=''>
+      <div>
         <div className='ticket-headings'>
           <div>Date</div>
           <div>Product</div>
