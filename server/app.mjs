d29dcd2b7853ef8cb6cbe11d6785828f897a52fe
Agustin Lozano Blua <agustinlozanoblua@gmail.com>
2022-07-20 08:59:28 -0300

    Trying to fix deployment

--- server/app.mjs
+++ server/app.mjs
@@ -14,7 +14,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(requestLogger)
 
-app.use(express.static('../frontend/dist'))
+app.use(express.static('./dist'))
 
 // Routes
 app.use('/api/users', userRoutes)
