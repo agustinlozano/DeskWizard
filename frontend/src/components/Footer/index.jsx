9b63ab1b1b531597c9271c5b77d3bce502c3d09e
Agustin Lozano Blua <agustinlozanoblua@gmail.com>
2022-07-22 19:41:15 -0300

    Add HTML attributes to the Footer component

--- frontend/src/components/Footer/index.jsx
+++ frontend/src/components/Footer/index.jsx
@@ -7,20 +7,42 @@ const Footer = () => {
     <div className='footer'>
       <div className='developer'>
         <p>Developed by
-          <a href='https://www.linkedin.com/in/agustin-lozano-blua/'>
+          <a
+            href='https://www.linkedin.com/in/agustin-lozano-blua/'
+            target='_blank'
+            rel='noreferrer'
+          >
             <span>&nbsp;Agustin Lozano</span>
           </a>
         </p>
       </div>
       <ul className='social-media'>
         <li>
-          <a href='https://github.com/agustinlozano'><AiFillGithub /></a>
+          <a
+            href='https://github.com/agustinlozano'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <AiFillGithub />
+          </a>
         </li>
         <li>
-          <a href='https://www.linkedin.com/in/agustin-lozano-blua/'><AiFillLinkedin /></a>
+          <a
+            href='https://www.linkedin.com/in/agustin-lozano-blua/'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <AiFillLinkedin />
+          </a>
         </li>
         <li>
-          <a href='mailto:agustinlozanoblua@gmail.com'><SiGmail /></a>
+          <a
+            href='mailto:agustinlozanoblua@gmail.com'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <SiGmail />
+          </a>
         </li>
       </ul>
     </div>
