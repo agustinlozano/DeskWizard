b56cfd139541cf32410c06abda08e1c2b901a701
Agustin Lozano Blua <agustinlozanoblua@gmail.com>
2022-07-21 22:31:01 -0300

    Update App according to the changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,9 +7,11 @@ import { ToastContainer } from 'react-toastify'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import Tickets from './pages/Tickets'
+import Ticket from './pages/Ticket'
+import TicketError from './pages/TicketError'
 import PageNotFound from './pages/PageNotFound'
 import NewTicket from './pages/NewTicket'
-import Tickets from './pages/Tickets'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import PrivateRoute from './components/PrivateRoute'
@@ -34,6 +36,11 @@ function App () {
               <Route path='/tickets' element={<Tickets />} />
             </Route>
 
+            <Route path='/ticket/:ticketId' element={<PrivateRoute />}>
+              <Route path='/ticket/:ticketId' element={<Ticket />} />
+            </Route>
+
+            <Route path='ticket-error' element={<TicketError />} />
             <Route path='*' element={<PageNotFound />} />
           </Routes>
         </div>
